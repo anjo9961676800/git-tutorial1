d6a5fe3d5009dd9cb859fbb90935c4dabed52aa3
user <geethudevassykutty56@gmail.com>
2018-04-18 04:52:37 -0400

    create a funtion to add three numbers

--- add.py
+++ add.py
@@ -1,5 +1,13 @@
-def add2(n1, n2):
-			s = n1 + n2
-			return s
- print add2(3, 4)
+def add2(n1,n2):
+	s=n1+n2
+	return s
+print add2(3,4)
+
+
+def add(n1,n2,n3):
+	r=n1+n2+n3
+	return r
+print add(3,4,5)
+
+
 	
